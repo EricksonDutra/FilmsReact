e0786a86b5dd70ee09c12dec6c4e54be05fbb7ba
EricksonDutra <ericksond10@gmail.com>
2023-01-08 12:46:25 -0400

    Deploy aplicação

---
+++ src/pages/Login.tsx
@@ -0,0 +1,11 @@
+import * as Styles from "../styles/pages/Home"
+
+export function Login() {
+ return (
+  <Styles.Container>
+    <div className="login">
+    <h1>LOGIN</h1>
+    </div>
+  </Styles.Container>
+ )
+}
